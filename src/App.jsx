0c17a606834066ca82ca98264d3103004bca10d0
AnandS1807 <mranand0709@gmail.com>
2025-03-10 16:07:02 +0530

    updated local changes before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage';
+
 import Dashboard from './pages/Dashboard';
 import SprintsPage from './pages/Sprint_section'
 import Task_dashboard from './pages/Task_dashboard'
@@ -8,13 +11,29 @@ import RetrospectiveComponent from './pages/Retrospective'
 import NotificationsDemo from './components/notitication'
 import Feed from './components/update_feed'
 import './index.css';
-
+import Login from './pages/Login';
+import SignIn from './pages/SignIn';
+import ForgotPassword from "./pages/ForgotPassword";
+import PurposeSelection from "./pages/PurposeSelection";
+import TeamInvite from "./pages/TeamInvite";
+import SprintTasks from "./pages/SprintTasks";
+import Integrations from "./pages/Integrations";
+import TaskBoardColumns from "./pages/TaskBoardColumns";
 
 function App() {
     return (
         <Router>
           <Routes>
-          <Route path="/" element={<Dashboard/>} />
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/SignIn" element={<SignIn/>} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/purpose-selection" element={<PurposeSelection />} />
+          <Route path="/TeamInvite" element={<TeamInvite />} />
+          <Route path="/SprintTasks" element={<SprintTasks />} />
+          <Route path="/Integrations" element={<Integrations/>} />
+          <Route path="/TaskBoardColumns" element={<TaskBoardColumns/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="/sprintspage" element={<SprintsPage/>} />
           <Route path="/taskdashboard" element={<Task_dashboard/>} />
           <Route path="/bugsqueue" element={<Bugs_queue_section/>} />
